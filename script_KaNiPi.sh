68edbb760279d0fd00587d364c523d2ce2f00e94
pgosselin92 <100447321+pgosselin92@users.noreply.github.com>
2022-03-25 05:43:35 +0100

    Version finale - Suppression commentaires inutiles

--- script_KaNiPi.sh
+++ script_KaNiPi.sh
@@ -1,42 +1,20 @@
 #!/bin/bash
-
-##########################
-##########################
-# //////// GROUPS ////////
-##########################
-##########################
-
 groupadd -g 10000 Directeur
 groupadd -g 11000 Recherche
 groupadd -g 12000 Developpement
 groupadd -g 13000 Ingenierie
 groupadd -g 16666 Techtech_admins
 
-#########################
-#########################
-# //////// USERS ////////
-#########################
-#########################
-
 useradd -mc "Bill Porte" -u 10000 -g 10000 -G 11000,12000,13000,16666 BPo
-#############################
-# Utilisateurs == [Recherche] 
-#############################
 useradd -mc "Robert Duval" -u 11000 -g 11000 -G 16666 RDu
 useradd -mc "Sam Francois" -u 11001 -g 11000 SFr
 useradd -mc "Albert Chamfort" -u 11002 -g 11000 ACh
 useradd -mc "Jeanne Latour" -u 11003 -g 11000 JLa
 useradd -mc "Sonia Andersen" -u 11004 -g 11000 SAn
-#################################
-# Utilisateurs == [Developpement]
-#################################
 useradd -mc "Nadine Dulks" -u 12000 -g 12000 -G 16666 NDu
 useradd -mc "Linus Turalds" -u 12001 -g 12000 LTu
 useradd -mc "Ken Thompson" -u 12002 -g 12000 KTh
 useradd -mc "Jacques Demaitre" -u 12003 -g 12000 JDe
-##############################
-# Utilisateurs == [Ingenierie]
-##############################
 useradd -mc "Herman Stamper" -u 13000 -g 13000 -G 16666 HSt
 useradd -mc "Rose Martin" -u 13001 -g 13000 RMa
 useradd -mc "Alfred Demaison" -u 13002 -g 13000 ADe
@@ -55,57 +33,34 @@ echo "hermanmdp" | sudo passwd --stdin HSt
 echo "rosemdp" | sudo passwd --stdin RMa
 echo "alfredmdp" | sudo passwd --stdin ADe
 
-###############################
-###############################
-# //////// DIRECTORIES ////////
-###############################
-###############################
 chmod -R 0755 /home 
 
 sudo -u BPo mkdir -m 1744 /home/BPo/public
-##########################
-# Repertoires == Recherche
-##########################
 sudo -u RDu mkdir -m 1744 /home/RDu/public
 sudo -u RDu mkdir -m 0760 /home/RDu/group_dir
 sudo -u RDu mkdir -m 0660 /home/RDu/admin && chgrp 16666 /home/RDu/admin
-
 sudo -u SFr mkdir -m 1744 /home/SFr/public
 sudo -u SFr mkdir -m 0760 /home/SFr/group_dir
-
 sudo -u ACh mkdir -m 1744 /home/ACh/public
 sudo -u ACh mkdir -m 0760 /home/ACh/group_dir
-
 sudo -u JLa mkdir -m 1744 /home/JLa/public
 sudo -u JLa mkdir -m 0760 /home/JLa/group_dir
-
 sudo -u SAn mkdir -m 1744 /home/SAn/public
 sudo -u SAn mkdir -m 0760 /home/SAn/group_dir
-##############################
-# Repertoires == Developpement
-##############################
 sudo -u NDu mkdir -m 1744 /home/NDu/public
 sudo -u NDu mkdir -m 0760 /home/NDu/group_dir
 sudo -u NDu mkdir -m 0660 /home/NDu/admin && chgrp 16666 /home/NDu/admin
-
 sudo -u LTu mkdir -m 1744 /home/LTu/public
 sudo -u LTu mkdir -m 0760 /home/LTu/group_dir
-
 sudo -u KTh mkdir -m 1744 /home/KTh/public
 sudo -u KTh mkdir -m 0760 /home/KTh/group_dir
-
 sudo -u JDe mkdir -m 1744 /home/JDe/public
 sudo -u JDe mkdir -m 0760 /home/JDe/group_dir
-###########################
-# Repertoires == Ingenierie
-###########################
 sudo -u HSt mkdir -m 1744 /home/HSt/public
 sudo -u HSt mkdir -m 0760 /home/HSt/group_dir
 sudo -u HSt mkdir -m 0660 /home/HSt/admin && chgrp 16666 /home/HSt/admin
-
 sudo -u RMa mkdir -m 1744 /home/RMa/public
 sudo -u RMa mkdir -m 0760 /home/RMa/group_dir
-
 sudo -u ADe mkdir -m 1744 /home/ADe/public
 sudo -u ADe mkdir -m 0760 /home/ADe/group_dir
 
